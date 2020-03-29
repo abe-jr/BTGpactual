fe891129eb181a12aa6712e3f085cde1fd2c2ff5
Abe <abelardobandeirajr@gmail.com>
2020-03-29 14:32:45 -0300

    update

--- BTG/src/banking/BankTransfer.java
+++ BTG/src/banking/BankTransfer.java
@@ -5,172 +5,74 @@ import java.util.Scanner;
 public class BankTransfer {
 	
 	public static void main(String[] args) {
-		
-	}
-	
-	class Bank
-	{
-		private String accno;
-		private String name;
-		private long balance;
 
-		Scanner KB=new Scanner(System.in);
-		
-		//method to open an account
-		void openAccount()
-		{ 
-			System.out.print("Enter Account No: ");
-			accno=KB.next();
-			System.out.print("Enter Name: ");
-			name=KB.next();
-			System.out.print("Enter Balance: ");
-			balance=KB.nextLong();
-		}
-
-		//method to display account details
-		void showAccount()
-		{ 
-			System.out.println(accno+","+name+","+balance);
-		}
-
-		//method to deposit money
-		void deposit()
+		class Bank
 		{
-			long amt;
-			System.out.println("Enter Amount U Want to Deposit : ");
-			amt=KB.nextLong();
-			balance=balance+amt;
-		}
+			private String conta;
+			private String nome;
+			private long saldo;
 
-		//method to withdraw money
-		void withdrawal()
-		{
-			long amt;
-			System.out.println("Enter Amount U Want to withdraw : ");
-			amt=KB.nextLong();
-			if(balance>=amt)
-			{ 
-				balance=balance-amt;
-			}
-			else
-			{
-				System.out.println("Less Balance..Transaction Failed..");
-			}
-		}
-
-		//method to search an account number
-		boolean search(String acn)
-		{ 
-			if(accno.equals(acn))
-			{ 
-				showAccount();
-				return(true);
-			}
-			return(false);
-		}
-	}
-
-	class ExBank
-	{  
-		public static void main(String arg[])
-		{
 			Scanner KB=new Scanner(System.in);
 			
-			//create initial accounts
-			System.out.print("How Many Customer U Want to Input : ");
-			int n=KB.nextInt();
-			Bank  C[]=new Bank[n];
-			for(int i=0;i<C.length;i++)
-			{   
-				C[i]=new Bank();
-				C[i].openAccount();
-			}
+			// abrir conta
 			
-			//run loop until menu 5 is not pressed
-			int ch;
-			do
+			void openAccount()
+			{ 
+				System.out.print("Informe o número da conta: ");
+				conta=KB.next();
+				System.out.print("Informe seu nome: ");
+				nome=KB.next();
+				System.out.print("Informe o saldo: ");
+				saldo=KB.nextLong();
+			}
+
+			// mostrar detalhes da conta
+			void showAccount()
+			{ 
+				System.out.println(conta+","+nome+","+saldo);
+			}
+
+			// depositar dinheiro
+			void transfer()
 			{
-				System.out.println("Main Menu\n
-				1.Display All\n
-				2.Search By Account\n
-				3.Deposit\n
-				4.Withdrawal\n
-				5.Exit");
-				System.out.println("Ur Choice :");
-				ch=KB.nextInt();
-				switch(ch)
+				long amt;
+				System.out.println("Digite o valor que vai transferir : ");
+				amt=KB.nextLong();
+				saldo=saldo+amt;
+			}
+
+			// receber transferencia
+			void receipt()
+			{
+				long amt;
+				System.out.println("Enter Amount U Want to withdraw : ");
+				amt=KB.nextLong();
+				if(saldo>=amt)
 				{ 
-					case 1:
-						for(int i=0;i<C.length;i++)
-						{
-							C[i].showAccount();
-						}
-						break;
-
-					case 2:
-						System.out.print("Enter Account No U Want to Search...: ");
-						String acn=KB.next();
-						boolean found=false;
-						for(int i=0;i<C.length;i++)
-						{  
-							found=C[i].search(acn);
-							if(found)
-							{
-								break;
-							}
-						}
-						if(!found)
-						{
-							System.out.println("Search Failed..Account Not Exist..");
-						}
-						break;
-
-					case 3:
-						System.out.print("Enter Account No : ");
-						acn=KB.next();
-						found=false;
-						for(int i=0;i<C.length;i++)
-						{  
-							found=C[i].search(acn);
-							if(found)
-							{
-								C[i].deposit();
-								break;
-							}
-						}
-						if(!found)
-						{
-							System.out.println("Search Failed..Account Not Exist..");
-						}
-						break;
-
-					case 4:
-						System.out.print("Enter Account No : ");
-						acn=KB.next();
-						found=false;
-						for(int i=0;i<C.length;i++)
-						{  
-							found=C[i].search(acn);
-							if(found)
-							{
-								C[i].withdrawal();
-								break;
-							}
-						}
-						if(!found)
-						{
-							System.out.println("Search Failed..Account Not Exist..");
-						}
-						break;
-
-					case 5:
-						System.out.println("Good Bye..");
-						break;
+					saldo=saldo-amt;
+				}
+				else
+				{
+					System.out.println("Less Balance..Transaction Failed..");
 				}
 			}
-			while(ch!=5);
+
+			//method to search an account number
+			boolean search(String acn)
+			{ 
+				if(conta.equals(acn))
+				{ 
+					showAccount();
+					return(true);
+				}
+				return(false);
+			}
 		}
 	}
+}
 	
 
-}
+	
+	
+	
+	
